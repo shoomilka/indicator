d2217b351b5c18d62833d54d6c48ca9fdba8395d
shoomilka <shoomilka@i.ua>
2020-02-14 12:33:58 +0200

    indecator added

--- src/router.js
+++ src/router.js
@@ -1,6 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css';
+
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+   {path: '/', name: 'home', component: Home},
+   {path: '/about', name: 'about', component: About}
+ ]
+})
+
+router.beforeResolve((to, from, next) => {
+	if(to.path){
+		NProgress.start()
+	}
+	next()
+})
+
+router.afterEach((to, from) => {
+	NProgress.done()
+})
+
+export default router
+/*
 
 Vue.use(Router)
 
@@ -17,7 +45,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" *//* './views/About.vue')
     }
   ]
 })
+*/
